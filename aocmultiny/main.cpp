50b69e0bd0c04fa85413cf10f6d16982d1caf06d
René Kooi <renee@kooi.me>
2016-05-29 13:28:44 +0200

    connect to IRC when not testing host/join

--- aocmultiny/main.cpp
+++ aocmultiny/main.cpp
@@ -1,7 +1,9 @@
+#include <map>
 #include <string>
 #include <sstream>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include "main.hpp"
 #include "Lobby.hpp"
 #include "irc/IRC.hpp"
@@ -22,6 +24,27 @@ void main (vector<string> params) {
   } else if (action == "join") {
     Lobby lobby;
     lobby.join({ 0 }, params[1]);
+  } else {
+    wcout << "[main] IRC" << endl;
+    auto irc = new irc::IRC("localhost");
+    irc->nick("AoCMulTiny");
+    irc->on({
+      { "NICK", [] (irc::IRC* irc, vector<string> params) {
+        wcout << "[main] NICK ok" << endl;
+      } },
+      { "433", [] (irc::IRC* irc, vector<string> params) {
+        wcout << "[main] Nickname is in use" << endl;
+      } }
+    });
+    irc->list();
+    irc->on("323", [] (irc::IRC* irc, vector<string> params) {
+      for_each(irc->channels.begin(), irc->channels.end(), [] (string channel) {
+        cout << channel << " ";
+      });
+      wcout << endl;
+    });
+    getchar();
+    delete irc;
   }
 
   wcout << "[main] Exiting" << endl;
