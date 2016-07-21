14740cc879c7706305f19fe98a77e48f4d38716b
René Kooi <renee@kooi.me>
2016-07-21 20:39:25 +0200

    add AoC multiplayer preset data fun

--- src/dplib/DPGameAoC.cpp
+++ src/dplib/DPGameAoC.cpp
@@ -8,6 +8,24 @@ using namespace std;
 namespace aocmultiny {
 namespace dplib {
 
+DPGameAoC::DPGameAoC ()
+    :
+    DPGame(GUID_AoC) {
+  this->preset = static_cast<DPGameAoCPresetData*>(
+    calloc(1, sizeof(DPGameAoCPresetData))
+  );
+  this->preset->u2[0] = 13;
+  this->preset->u2[2] = 1;
+  this->preset->u2[4] = 1;
+  this->preset->u3[0] = 3;
+  // Record games by default.
+  this->preset->recordGame = 1;
+}
+
+DPGameAoC::~DPGameAoC () {
+  free(this->preset);
+}
+
 void DPGameAoC::receiveMessage (DPLobbyMessage* message) {
   if (message->flags == DPLMSG_STANDARD) {
     wcout << "[DPGameAoC::receiveMessage] received STANDARD message, discarding" << endl;
@@ -18,25 +36,38 @@ void DPGameAoC::receiveMessage (DPLobbyMessage* message) {
   wcout << "[DPGameAoC::receiveMessage] received SYSTEMMESSAGE, processing" << endl;
   switch (sysMsg->dwType) {
   case DPLSYS_APPTERMINATED:
-    wcout << "[DPGameAoC::receiveMessage] received APPTERMINATED message" << endl
-          << "Press <enter> to exit." << endl;
+    wcout << "[DPGameAoC::receiveMessage] received APPTERMINATED message" << endl;
     lobby->onAppTerminated.emit();
     message->stop();
     return;
   case DPLSYS_GETPROPERTY: {
     wcout << "[DPGameAoC::receiveMessage] received GETPROPERTY message" << endl;
     auto getPropMsg = static_cast<DPLMSG_GETPROPERTY*>(message->data);
-    auto responseSize = sizeof(DPLMSG_GETPROPERTYRESPONSE);
-    auto getPropRes = static_cast<DPLMSG_GETPROPERTYRESPONSE*>(malloc(responseSize));
-    getPropRes->dwType = DPLSYS_GETPROPERTYRESPONSE;
-    getPropRes->dwRequestID = getPropMsg->dwRequestID;
-    getPropRes->guidPlayer = getPropMsg->guidPlayer;
-    getPropRes->guidPropertyTag = getPropMsg->guidPropertyTag;
-    getPropRes->hr = DPERR_UNKNOWNMESSAGE;
-    getPropRes->dwDataSize = 0;
-    getPropRes->dwPropertyData[0] = 0;
-    wcout << "[DPGameAoC::receiveMessage] responding to unknown GETPROPERTY message" << endl;
-    message->reply(getPropRes, responseSize);
+    if (IsEqualGUID(getPropMsg->guidPropertyTag, GUID_AoCPresetData)) {
+      auto responseSize = sizeof(DPLMSG_GETPROPERTYRESPONSE) - sizeof(DWORD) + sizeof(DPGameAoCPresetData);
+      auto getPropRes = static_cast<DPLMSG_GETPROPERTYRESPONSE*>(malloc(responseSize));
+      getPropRes->dwType = DPLSYS_GETPROPERTYRESPONSE;
+      getPropRes->dwRequestID = getPropMsg->dwRequestID;
+      getPropRes->guidPlayer = getPropMsg->guidPlayer;
+      getPropRes->guidPropertyTag = getPropMsg->guidPropertyTag;
+      getPropRes->hr = DP_OK;
+      getPropRes->dwDataSize = sizeof(DPGameAoCPresetData);
+      memcpy(getPropRes->dwPropertyData, this->preset, sizeof(DPGameAoCPresetData));
+      wcout << "[DPGameAoC::receiveMessage] responding to Preset Data GETPROPERTY message " << endl;
+      message->reply(getPropRes, responseSize);
+    } else {
+      auto responseSize = sizeof(DPLMSG_GETPROPERTYRESPONSE);
+      auto getPropRes = static_cast<DPLMSG_GETPROPERTYRESPONSE*>(malloc(responseSize));
+      getPropRes->dwType = DPLSYS_GETPROPERTYRESPONSE;
+      getPropRes->dwRequestID = getPropMsg->dwRequestID;
+      getPropRes->guidPlayer = getPropMsg->guidPlayer;
+      getPropRes->guidPropertyTag = getPropMsg->guidPropertyTag;
+      getPropRes->hr = DPERR_UNKNOWNMESSAGE;
+      getPropRes->dwDataSize = 0;
+      getPropRes->dwPropertyData[0] = 0;
+      wcout << "[DPGameAoC::receiveMessage] responding to unknown GETPROPERTY message" << endl;
+      message->reply(getPropRes, responseSize);
+    }
     break;
   }
   case DPLSYS_NEWSESSIONHOST:
--- src/dplib/DPGameAoC.hpp
+++ src/dplib/DPGameAoC.hpp
@@ -4,9 +4,77 @@
 namespace aocmultiny {
 namespace dplib {
 
+// {B3F2E132-FE6A-11D2-8DEE-00A0C90832B4}
+const GUID GUID_AoCPresetData = { 0xB3F2E132, 0xFE6A, 0x11D2, { 0x8D, 0xEE, 0x00, 0xA0, 0xC9, 0x08, 0x32, 0xB4 } };
+
 // {5DE93F3F-FC90-4ee1-AE5A-63DAFA055950}
-const GUID guidAoC = { 0x5DE93F3F, 0xFC90, 0x4ee1, { 0xAE, 0x5A, 0x63, 0xDA, 0xFA, 0x05, 0x59, 0x50 } };
-DEFINE_DPGAME(DPGameAoC, guidAoC);
+const GUID GUID_AoC = { 0x5DE93F3F, 0xFC90, 0x4ee1, { 0xAE, 0x5A, 0x63, 0xDA, 0xFA, 0x05, 0x59, 0x50 } };
+
+#pragma pack(1)
+struct DPGameAoCPresetData {
+  uint8_t civAvail[20];
+  uint8_t lockCivAvail;
+  uint8_t gameType;
+  uint8_t lockGameType;
+
+  union {
+    char gameFilename[260];
+    struct {
+      char upGameFilename[240];
+      DWORD specIps[4];
+      DWORD reservedFlags;
+    };
+  };
+
+  uint8_t mapTypeAvail[39];
+  uint8_t lockMapTypeAvail;
+  uint8_t mapSize;
+  uint8_t lockMapSize;
+  uint8_t difficulty;
+  uint8_t lockDifficulty;
+  uint8_t resources;
+  uint8_t lockResources;
+  uint8_t u0[3];
+  DWORD population;
+  uint8_t lockPopulation;
+  uint8_t gameSpeed;
+  uint8_t lockGameSpeed;
+  uint8_t startingAge;
+  uint8_t lockStartingAge;
+  uint8_t victory;
+  uint8_t lockVictory;
+  uint8_t u1;
+  DWORD timeScore;
+  uint8_t lockTimeScore;
+  uint8_t teamTogether;
+  uint8_t lockTeamTogether;
+  uint8_t teamsLocked;
+  uint8_t lockTeamsLocked;
+  uint8_t speedLocked;
+  uint8_t lockSpeedLocked;
+  uint8_t recordGame;
+  uint8_t lockRecordGame;
+  uint8_t revealMap;
+  uint8_t lockRevealMap;
+  uint8_t allTechs;
+  uint8_t lockAllTechs;
+  uint8_t allowCheats;
+  uint8_t lockAllowCheats;
+  uint8_t u2[6];
+  uint8_t teamBonuses;
+  uint8_t lockTeamBonuses;
+  uint8_t u3[5];
+};
+
+class DPGameAoC: public DPGame {
+public:
+  DPGameAoCPresetData* preset;
+
+  DPGameAoC ();
+  ~DPGameAoC ();
+
+  void receiveMessage (DPLobbyMessage* message);
+};
 
 }
 }
