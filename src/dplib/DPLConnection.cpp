54965c228db2902a2a4e27b7924da1825b776de2
René Kooi <renee@kooi.me>
2016-08-15 19:51:07 +0200

    dplib: use Service Provider stored in the DPAddress instead of hardcoding DPSP_NICE

--- src/dplib/DPLConnection.cpp
+++ src/dplib/DPLConnection.cpp
@@ -10,8 +10,6 @@ using std::tie;
 
 namespace dplib {
 
-const GUID DPSPGUID_NICE = { 0xe2dd8ebe, 0x1f03, 0x43b7, { 0x8d, 0x92, 0x9c, 0x6c, 0x2f, 0x5c, 0x44, 0x26 } };
-
 DPLConnection::DPLConnection (DPAddress* address, DPSessionDesc* sessionDesc, DPName* playerName)
     :
     address(address),
@@ -27,7 +25,8 @@ void DPLConnection::alloc () {
     DPLCONNECTION_CREATESESSION : DPLCONNECTION_JOINSESSION;
   connection->lpSessionDesc = this->sessionDesc->unwrap();
   connection->lpPlayerName = this->name->unwrap();
-  connection->guidSP = DPSPGUID_NICE;
+  auto spPart = this->address->get(DPAID_ServiceProvider);
+  connection->guidSP = *static_cast<GUID*>(spPart.first);
 
   tie(
     connection->lpAddress,
