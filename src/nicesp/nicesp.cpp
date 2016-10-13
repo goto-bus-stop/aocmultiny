70e1274297e289043cc0ad1fd3dd7d47dbb79d89
René Kooi <renee@kooi.me>
2016-10-13 12:23:45 +0200

    nice: send log output to a file

--- src/nicesp/nicesp.cpp
+++ src/nicesp/nicesp.cpp
@@ -4,6 +4,7 @@
 #include <gio/gnetworking.h>
 #include <ctime>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <map>
 #include "debug.hpp"
@@ -476,11 +477,45 @@ __declspec(dllexport) HRESULT __cdecl SPInit (SPINITDATA* spData) {
   return nicesp::init(spData);
 }
 
+G_LOCK_DEFINE(loglock);
+static void logToFile (const gchar* domain, GLogLevelFlags, const gchar* message, void* ctx) {
+  auto output = static_cast<ofstream*>(ctx);
+  G_LOCK(loglock);
+  if (domain != NULL) {
+    *output << domain << "  " << message << endl;
+  }
+  if (message != NULL) {
+    *output << "glog " << message << endl;
+  }
+  G_UNLOCK(loglock);
+}
+
+streambuf* coutbuf;
+ofstream logfile;
+static void setupLogging () {
+  logfile.open("C:\\nicesp-" + to_string(time(0)) + ".txt");
+  // Send g_log messages to file
+  g_log_set_default_handler(logToFile, &logfile);
+  // Redirect cout to log file
+  coutbuf = cout.rdbuf();
+  cout.rdbuf(logfile.rdbuf());
+}
+static void teardownLogging () {
+  cout.rdbuf(coutbuf);
+  logfile.close();
+}
+
 BOOL WINAPI DllMain (HINSTANCE instance, DWORD reason, void* reserved) {
   g_debug("DllMain (0x%p, %ld, %p)", instance, reason, reserved);
 
   switch (reason) {
   case DLL_PROCESS_ATTACH:
+    setupLogging();
+
+    g_message("--------------------");
+    g_message("[DllMain] INITIALISE");
+    g_message("--------------------");
+
     CoCreateInstance(
       CLSID_DirectPlayLobby,
       NULL,
@@ -492,6 +527,12 @@ BOOL WINAPI DllMain (HINSTANCE instance, DWORD reason, void* reserved) {
     DisableThreadLibraryCalls(instance);
     break;
   case DLL_PROCESS_DETACH:
+    g_message("-----------------");
+    g_message("[DllMain] RELEASE");
+    g_message("-----------------");
+
+    teardownLogging();
+
     nicesp::_lobby->Release();
     break;
   default:
